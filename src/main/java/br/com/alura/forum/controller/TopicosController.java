b8ef17eb157b1c4be4df9ebab32dcb47b32a6170
Danilo Ferreira <danilocsf@gmail.com>
2021-07-19 14:53:30 -0300

    Adicionado cache e Security

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -93,10 +94,15 @@ public class TopicosController {
 	 * e que devem ser informados em TopicoForm.
 	 * 
 	 * UriComponentsBuilder será injetado pelo Spring e conterá a URI completa da requisição*/
-	@PostMapping
+	@PostMapping	
+	 /*
+	  * @CacheEvict - Para dizer que quero que o Spring limpe determinado cache
+	  * allEntries - para limpar tudo deste cache
+	  */
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)	
+	@Transactional
 	/*O ResponseEntity é utilizado para retornar o corpo da resposta (contido no objeto TopicoDto) e pra informar
 	 * o código do retorno - no caso 201*/
-	@Transactional
 	public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
@@ -111,6 +117,7 @@ public class TopicosController {
 	/*
 	 * Utiliza uma nova classe para atualizacao pois pode ter dados que não devem ser alterados no objeto form de criação*/
 	@PutMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	@Transactional
 	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form, UriComponentsBuilder uriBuilder) {
 		Optional<Topico> optTopico = topicoRepository.findById(id);
@@ -124,6 +131,7 @@ public class TopicosController {
 	
 	
 	@DeleteMapping("/{id}")
+	@CacheEvict(value = "listaDeTopicos", allEntries = true)
 	@Transactional
 	public ResponseEntity<?> remover(@PathVariable Long id) {
 		Optional<Topico> optTopico = topicoRepository.findById(id);
